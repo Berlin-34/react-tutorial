a0e47160f4d490e8537000101ddd30be7e9bb4fb
Berlin-34 <72955795+Berlin-34@users.noreply.github.com>
2024-03-14 22:52:35 +0530

    refactored code

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    title: "Ikigai",
+    author: "Francesc Miralles",
+    img: "./images/book-1.jpg",
+    id: 1,
+  },
+  {
+    title: "The Psychology of Money",
+    author: "Morgan Housel",
+    img: "./images/book-2.jpg",
+    id: 2,
+  },
+  {
+    title: "Courage To Be Dislikeds",
+    author: "Ichiro Kishimi and Fumitake Koga",
+    img: "./images/book-3.jpg",
+    id: 3,
+  },
+];
